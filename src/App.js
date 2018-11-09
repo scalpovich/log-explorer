64b9743be70113f52c6b979d18c051c793738976
Naved Rangwala <narangwa@microsoft.com>
2018-11-09 11:01:39 -0800

    initial add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import ReactDOM from "react-dom";
+import FileView from "./FileView";
+const {remote} = window.require('electron');
+
+const dialog = remote.dialog;
+
+window.require('electron').ipcRenderer.on('keyPress', (event, message) => {
+  switch (message) {
+    case 'CommandOrControl+O':
+      openFileDialog();
+      break;
+    default:
+      break;
+  }
+});
+
+function openFileDialog() {
+  dialog.showOpenDialog(fileNames => {
+    if (fileNames === undefined) {
+      return;
+    }
+    ReactDOM.render(<FileView fileName={fileNames[0]} />, document.getElementById('root'));
+  });
+}
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="open-file-container">
+        <h1>Log Analyzer</h1>
+        <input type="button" id="btn-readfile" value="Click to select file" onClick={openFileDialog} />
       </div>
     );
   }
